23bcf70fc914fb95d175e898016d0cb9deefe842
JSPDeveloper <stivengarcia10.sg@gmail.com>
2025-05-09 14:04:40 -0500

    Empaquetado de dependencias

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup (
+    name="edu_pad",
+    version="0.0.1",
+    #version componente.version funcionalidad.version cambio
+    author="Jeison Stiven Parra Garcia- Jefferson SanJuan Ortiz",
+    author_email="jeison.parra@est.iudigital.edu.co - ",
+    description="Proyecto Scraping para IU Digital",
+    py_modules=["actividad1","actividad2"],
+    install_requieres=[
+        "pandas",
+        "openpyxl",
+        "requests",
+        "beautifulsoup4"   
+    ]
+)
