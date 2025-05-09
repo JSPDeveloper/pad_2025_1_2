3e2d89d87abad69dbcccae54c7c3808742adef6a
JSPDeveloper <stivengarcia10.sg@gmail.com>
2025-05-09 16:17:57 -0500

    Se crea clase DataWeb

---
+++ src/edu_pad/dataweb.py
@@ -0,0 +1,14 @@
+
+
+class DataWeb:
+    def __init__(self):
+        self.url="https://webscraper.io/test-sites/e-commerce/static/computers/laptops"
+        #https://www.falabella.com.pe/falabella-pe/category/cat16470475/Computadores-de-escritorio
+        #testId-searchResults-products
+
+        #https://webscraper.io/test-sites/e-commerce/static/computers/laptops
+        #col-lg-9
+
+dw=DataWeb()
+print(dw.url)
+        
