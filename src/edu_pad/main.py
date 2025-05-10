609d8b3181c9876e350675e1c8b10e464d7a94d3
JSPDeveloper <stivengarcia10.sg@gmail.com>
2025-05-10 00:34:16 -0500

    Eliminaciión de archivo mal creado

--- src/edu_pad/main.py
+++ src/edu_pad/main.py
@@ -3,10 +3,8 @@ import pandas as pd
 
 def main():
     dataweb=DataWeb()
-    df=pd.DataFrame() 
-    df=dataweb.obtenerdatos()
-    df.to_csv("DataWebPremierLeague.csv")
-
+    df = pd.DataFrame()
+    df = dataweb.obtener_datos()
+    df.to_csv("DataWebPremierLeague.csv",index=False)
 if __name__ == "__main__":
-    main()    
-    
+    main()
